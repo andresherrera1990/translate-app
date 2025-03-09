356c3d8f6609f8aa0fbe52b689402b5600d83ecf
Andrés Herrera <andrewho198732@gmail.com>
2025-03-09 12:10:29 -0500

    Mi primer commit

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById("translateButton").addEventListener("click", async () => {
+    let text = document.getElementById("imputText").value;
+    if (text.trim() === "") return;
+
+    let response = await fetch (`https://api.mymemory.translated.net/get?q=${encodeURIComponent(text)}&langpair=es|en`);
+    let data = await response.json();
+
+    document.getElementById("outputText").value = data.responseData.translatedText;
+});
+
+document.getElementById("newTextButton").addEventListener("click", () => {
+    document.getElementById("imputText").value = "";
+    document.getElementById("outputText").value = "";    
+});
+
+document.getElementById("toggleMode").addEventListener("click", () => { 
+    document.body.classList.toggle("dark-mode");
+
+});
